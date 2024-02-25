7bea2099e0a1316c33c96b7648503d05cc28c716
fr98acsta <acosta98diaz@gmail.com>
2024-02-25 19:32:15 +0100

    Actualizado el index.js

--- party/index.js
+++ party/index.js
@@ -3,43 +3,49 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var maxGuest;
 maxGuest = 20;
 var guestsToAssign = Math.floor(Math.random() * maxGuest);
-actividad(guestsToAssign);
+celebration(guestsToAssign);
 console.log("\nAll done!");
-function actividad(nu) {
-    var activity; // Those were some nice type annotations you had here!
-    var capacity; // It'd be a shame if we ... *erased* them! 😈
-    var requiresSupplies;
+function celebration(nu) {
+    var party;
     var x;
     if (nu != 0) {
         x = Math.floor(Math.random() * nu + 1);
         if (x <= Math.floor(nu * 0.3 / 0.8)) {
-            activity = "balloon animals";
-            capacity = 5;
-            requiresSupplies = true;
+            party = {
+                activity: "balloon animals",
+                capacity: 5,
+                requiresSupplies: true,
+            };
         }
         else if (x <= Math.floor(nu * 0.2 / 0.8)) {
-            activity = "dancing";
-            capacity = 10;
-            requiresSupplies = false;
+            party = {
+                activity: "dancing",
+                capacity: 10,
+                requiresSupplies: true,
+            };
         }
         else if (x <= Math.floor(nu * 0.5 / 0.8)) {
-            activity = "juggling";
-            capacity = 3;
-            requiresSupplies = true;
+            party = {
+                activity: "juggling",
+                capacity: 3,
+                requiresSupplies: true,
+            };
         }
         else {
-            activity = "face painting";
-            capacity = 1;
-            requiresSupplies = true;
+            party = {
+                activity: "face painting",
+                capacity: 1,
+                requiresSupplies: true,
+            };
         }
-        if (nu - capacity >= 0) {
-            console.log("".concat(nu, " of us will enjoy ").concat(activity, "."));
-            console.log("\t".concat(capacity, " will enter the ").concat(activity, " activity."));
-            nu -= capacity;
-            actividad(nu);
+        if (nu - party.capacity >= 0) {
+            console.log("".concat(nu, " of us will enjoy ").concat(party.activity, "."));
+            console.log("\t".concat(party.capacity, " will enter the ").concat(party.activity, " activity."));
+            nu -= party.capacity;
+            celebration(nu);
         }
         else
-            actividad(nu);
+            celebration(nu);
     }
     // return [activity, capacity, requiresSupplies];
 }
